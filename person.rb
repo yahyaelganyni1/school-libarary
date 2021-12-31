f98c0e3762871ea3e8c7a5bdee20e7064297374b
yahya <crazy20046@gmail.com>
2021-12-31 11:18:59 +0200

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,3 +1,5 @@
+require_relative 'corrector'
+
 class Person
   attr_accessor :name, :age
   attr_reader :id
@@ -6,11 +8,16 @@ class Person
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @corrector = Corrector.new
     @parent_permission = parent_permission
   end
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def validate_name
+    @name = corrector.correct_name @name
   end
 
   private
