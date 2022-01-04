45a5694a2df3a50752fd203562ddc7189091cadc
yahya <crazy20046@gmail.com>
2022-01-04 11:06:09 +0200

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,28 +1,33 @@
-require_relative 'corrector'
-
+require_relative('./corrector')
 class Person
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1_000_000_000)
     @name = name
     @age = age
-    @corrector = Corrector.new
     @parent_permission = parent_permission
+    @corrector = Corrector.new
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental) unless @rentals.include?(rental)
+    rental.owner = self
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
   end
 
   def can_use_services?
     of_age? || @parent_permission
   end
 
-  def validate_name
-    @name = corrector.correct_name @name
-  end
-
   private
 
   def of_age?
-    @age >= 18
+    @age > 18
   end
 end
